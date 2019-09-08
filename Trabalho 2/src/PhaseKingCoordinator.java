a143fa7707f648df7b3ec98f616d73a67278f545
Eduardo Otte <eduardo@otte.net.br>
2019-09-08 17:18:21 -0300

    Update Phase King Coordinator

--- Trabalho 2/src/PhaseKingCoordinator.java
+++ Trabalho 2/src/PhaseKingCoordinator.java
@@ -4,21 +4,21 @@ import java.util.ArrayList;
 public class PhaseKingCoordinator {
     private final int numberOfProcesses = 5;
 
-    private ArrayList<PrivateKey> privateKeys = new ArrayList<PrivateKey>();
-    private ArrayList<PublicKey> publicKeys = new ArrayList<PublicKey>();
+    private ArrayList<PrivateKey> privateKeys = new ArrayList<>();
+    private ArrayList<PublicKey> publicKeys = new ArrayList<>();
 
-    private ArrayList<PhaseKingProcess> processes = new ArrayList<PhaseKingProcess>();
+    private ArrayList<PhaseKingProcess> processes = new ArrayList<>();
 
     public void begin() {
         generateKeyPairs();
 
         for(int i = 0; i < numberOfProcesses; i++) {
-            processes.add(new PhaseKingProcess(i, String.valueOf(i), 6789, "228.5.6.7",
+            processes.add(new PhaseKingProcess(i, "1", 6789, "228.5.6.7",
                     publicKeys, privateKeys.get(i)));
         }
 
         for(PhaseKingProcess p : processes) {
-            p.phaseKing();
+            p.start();
         }
     }
 
