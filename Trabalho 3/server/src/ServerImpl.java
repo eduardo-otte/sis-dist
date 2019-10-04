441ee6c0bdaab88deac04a1479e584ce048883c0
mdudapinho <mpinho@alunos.utfpr.edu.br>
2019-10-04 14:54:53 -0300

    implementations os some server side functions

--- Trabalho 3/server/src/ServerImpl.java
+++ Trabalho 3/server/src/ServerImpl.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 public class ServerImpl implements ServerInterface {
     // jobOfferings = {"company": {"area1": [], "area2": []}}
     private HashMap<String, Curriculum> curriculumHashMap;
+    private HashMap<String, JobOffering> jobOfferingHashMap;
 
     // Search
     public ArrayList<JobOffering> searchJobOffering(JobOffering jobOfferingLookup);
@@ -23,13 +24,74 @@ public class ServerImpl implements ServerInterface {
         curriculumHashMap.put(curriculum.getName(), curriculum);
         // return success
     }
+    /*key for jobOfferingHashMap is "[companyName]-[area]" */
+    public void registerJobOffering(JobOffering jobOffering){
+      string key = jobOffering.getCompanyName()+"-"+jobOffering.getArea();
 
-    public void registerJobOffering(JobOffering jobOffering);
+      if(jobOfferingHashMap.containsKey(key)) {
+          // return error
+      }
+
+      jobOfferingHashMap.put(key, jobOffering);
+      // return success
+
+    }
 
     // Alter registration
-    public void alterCurriculum(Curriculum newCurriculum);
+    public void alterCurriculum(Curriculum newCurriculum){
+        if(!curriculumHashMap.containsKey(curriculum.getName())) {
+            // return error - doesnt have this person's curriculum in the database
+        }
 
-    public void alterJobOffering(JobOffering newJobOffering);
+        /*
+        this.name = name; --Key : cant change
+        this.contact = contact;
+        this.area = area;
+        this.workload = workload;
+        this.intendedSalary = intendedSalary;
+        */
+
+        /*
+          I don't know if we'll send just what its gonna change, so....
+        */
+        Curriculum previousCurriculum = curriculumHashMap.get(curriculum.getName());
+        //if(newCurriculum.getcontact()!=null)
+        previousCurriculum.setContact(newCurriculum.getcontact());
+        //if(newCurriculum.getArea()!=null)
+        previousCurriculum.setArea(newCurriculum.getArea());
+        //if(newCurriculum.getWorkload()!=null)
+        previousCurriculum.setWorkload(newCurriculum.getWorkload());
+        //if(newCurriculum.getIntendedSalary()!=null)
+        previousCurriculum.setIntendedSalary(newCurriculum.getIntendedSalary());
+
+        // return success
+    }
+
+    public void alterJobOffering(JobOffering newJobOffering){
+      string key = newJobOffering.getCompanyName()+"-"+newJobOffering.getArea();
+      if(!jobOfferingHashMap.containsKey(key)) {
+          // return error- doesnt have this company's Job offer
+      }
+      /*
+      this.companyName = companyName; --Key : cant change
+      this.contact = contact;
+      this.area = area;               --Key : cant change
+      this.workload = workload;
+      this.salary = salary;
+      */
+
+      /*
+        I don't know if we'll send just what its gonna change, so....
+      */
+      JobOffering previousJobOffer = jobOfferingHashMap.get(key);
+      //if(newJobOffering.getContact()!=null)
+      previousJobOffer.setContact() = newJobOffering.getContact();
+      //if(newJobOffering.getWorkload()!=null)
+      previousJobOffer.setWorkload() = newJobOffering.getWorkload();
+      //if(newJobOffering.getSalary()!=null)
+      previousJobOffer.setSalary() = newJobOffering.getSalary();
+
+    }
 
     // Subscriptions
     public void subscribeForJobOfferings(ApplicantClientInterface clientRef, String areaOfInterest);
