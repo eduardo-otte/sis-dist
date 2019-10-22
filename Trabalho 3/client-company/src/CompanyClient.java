895b9a5619a7017d7490ac440f330dd83e410093
Eduardo Otte <eduardo.otte@ebanx.com>
2019-10-22 14:04:18 -0300

    Refactoring, bug fixes and error catching

--- Trabalho 3/client-company/src/CompanyClient.java
+++ Trabalho 3/client-company/src/CompanyClient.java
@@ -14,12 +14,12 @@ public class CompanyClient {
 
         boolean hasJobOffer = false;
         boolean mayEnd = false;
-        int option=-1;
-        String input = new String(); 
+        int option;
+        String input;
         
         Scanner keyboard = new Scanner(System.in);
         
-        ArrayList<JobOffering> jobsOffered = new ArrayList<JobOffering>();
+        ArrayList<JobOffering> jobsOffered = new ArrayList<>();
         
         try {
             Registry registry = LocateRegistry.getRegistry("localhost", 1099);
@@ -28,10 +28,6 @@ public class CompanyClient {
             CompanyClientImpl client = new CompanyClientImpl();
 
             System.out.println("Cliente inicializado");
-
-            //JobOffering jobOffering = new JobOffering("Blabla", "blablabla", "casa do caralho", 10, 10000);
-            //System.out.println("Oferta de emprego criada");
-            //serverInterface.registerJobOffering(jobOffering);
             
             while(!mayEnd) {
             	System.out.println("---------------------------");
@@ -41,109 +37,95 @@ public class CompanyClient {
 	            System.out.println("3)Checar currículos cadastrados");
 	            System.out.println("4)Cadastrar para envio de novos currículos");
 	            System.out.println("5)Encerrar");
-	            option = keyboard.nextInt();
+	            option = Parser.parseIntegerInput("");
 	            
-	           
-	            if(option==1 ) {
-	            	/*
-	                this.companyName = companyName; --Key : can't change
-	                this.contact = contact;
-	                this.area = area;               --Key : can't change
-	                this.workload = workload;
-	                this.salary = salary;
-	                */
-	            	System.out.println("Nome:");
-	            	input = keyboard.next();
+	           	// 1) Cadastra uma oferta de emprego
+	            if(option == 1 ) {
+	            	System.out.print("Nome: ");
+	            	input = keyboard.nextLine();
 	            	String companyName = input;
 	            	            	
-	            	System.out.println("Contato:");
-	            	input = keyboard.next();
+	            	System.out.print("Contato: ");
+	            	input = keyboard.nextLine();
 	            	String contact = input;
 	            	
-	            	System.out.println("Área de interesse:");
-	            	input = keyboard.next();
+	            	System.out.print("Área de interesse: ");
+	            	input = keyboard.nextLine();
 	            	String area = input;
-	            	
-	            	System.out.println("Carga horária:");
-	            	input = keyboard.next();
-	            	int workload = Integer.parseInt(input);
-	            	
-	            	System.out.println("Salário pretendido:");
-	            	input = keyboard.next();
-	            	float salary = Float.parseFloat(input);            	
+
+					int workload = Parser.parseIntegerInput("Carga horária: ");
+
+	            	float salary = Parser.parseFloatInput("Salário pretendido: ");
 	            	            	
-	            	hasJobOffer=true;
+	            	hasJobOffer = true;
 	            	JobOffering jobOffering = new JobOffering(companyName, contact, area, workload, salary);
 	            	jobsOffered.add(jobOffering);
 	                serverInterface.registerJobOffering(jobOffering);
 	                System.out.println("Oferta de emprego criada");
-	                
 	            }
-	            if(option==2 && !hasJobOffer) { //alterar dados de uma oferta de emprego
+
+	            if(option == 2 && !hasJobOffer) { //alterar dados de uma oferta de emprego
 	            	System.out.println("Você ainda não tem uma oferta de emprego cadastrada. Se você quiser enviar uma, escolha a opção 1");
 	            }
-	            if(option==2 && hasJobOffer) {
-	            	
-	            	int index=0;
-	            	if(jobsOffered.size()>1) {
-	            		System.out.println("Você tem "+ jobsOffered.size() + " ofertas de emprego cadastradas. Qual você deseja modificaar?");
+
+	            if(option == 2 && hasJobOffer) {
+	            	int index = 0;
+	            	if(jobsOffered.size() > 1) {
+	            		System.out.println("Você tem " + jobsOffered.size() + " ofertas de emprego cadastradas. Qual você deseja modificaar?");
 		            	for (int i=0; i<jobsOffered.size(); i++) {
 		            	    //companyName and workload can't change
-		            		System.out.println("Vaga "+ (i+1));
-		            		System.out.println(" - Área "+ jobsOffered.get(i).getArea());
-		            		System.out.println(" - Contato "+ jobsOffered.get(i).getContact());
-		            		System.out.println(" - Salário "+ jobsOffered.get(i).getSalary());
+		            		System.out.println("Vaga " + (i+1));
+		            		System.out.println(" - Área " + jobsOffered.get(i).getArea());
+		            		System.out.println(" - Contato " + jobsOffered.get(i).getContact());
+		            		System.out.println(" - Salário " + jobsOffered.get(i).getSalary());
 			          	}
-		            	input = keyboard.next();
-		            	index = Integer.parseInt(input);
+		            	index = Parser.parseIntegerInput("");
 	            	}
 	            	
-	            	System.out.println("Seu contato atual é "+ jobsOffered.get(index).getContact());
+	            	System.out.println("Seu contato atual é " + jobsOffered.get(index).getContact());
 	            	System.out.println("Você deseja modifica-lo? S/N");
-	            	input = keyboard.next().trim();
+	            	input = keyboard.nextLine().trim();
 	            	if(input.equals("S")) {
-	            		System.out.println("Digite seu novo contato:");
-	                	input = keyboard.next();
+	            		System.out.print("Digite seu novo contato: ");
+	                	input = keyboard.nextLine();
 	                	jobsOffered.get(index).setContact(input);
 	            	}
 	            	
-	            	System.out.println("Sua Área de interesse atual é "+ jobsOffered.get(index).getArea());
+	            	System.out.println("Sua Área de interesse atual é " + jobsOffered.get(index).getArea());
 	            	System.out.println("Você deseja modifica-la? S/N");
-	            	input = keyboard.next().trim();
+	            	input = keyboard.nextLine().trim();
 	            	if(input.equals("S")) {
-	            		System.out.println("Digite sua nova área de interesse:");
-	                	input = keyboard.next();
+	            		System.out.print("Digite sua nova área de interesse: ");
+	                	input = keyboard.nextLine();
 	                	jobsOffered.get(index).setArea(input);
 	            	}
 	            	
-	            	System.out.println("Seu salário atual é "+ jobsOffered.get(index).getSalary());
+	            	System.out.println("Seu salário atual é " + jobsOffered.get(index).getSalary());
 	            	System.out.println("Você deseja modifica-lo? S/N");
-	            	input = keyboard.next().trim();
+	            	input = keyboard.nextLine().trim();
 	            	if(input.equals("S")) {
-	            		System.out.println("Digite sua nova pretenção salarial? S/N");
-	                	input = keyboard.next();
-	                	jobsOffered.get(index).setSalary(Float.parseFloat(input));      
+	                	float newSalary = Parser.parseIntegerInput("Digite sua nova pretenção salarial: ");
+	                	jobsOffered.get(index).setSalary(newSalary);
 	            	}
 	            	
 	            	serverInterface.alterJobOffering(jobsOffered.get(index));
 	            	System.out.println("Sua oferta de emprego foi alterado");
 	            }
-	            else if (option==3) {	//3)Checar currículos cadastrados
-	            	
+
+				// 3) Checar currículos cadastrados
+	            else if (option == 3) {
 	            	//Consulta de vagas de emprego, indicando filtros como área de interesse
 	            	System.out.println("Qual a área de interesse a ser pesquisada?");
-	            	input = keyboard.next();
+	            	input = keyboard.nextLine();
 	            	String area = input;
-	            	
 	            	
 	            	ArrayList<Curriculum> curriculumAvaliable = serverInterface.searchCurriculum(area);
 	            	
 	            	if(curriculumAvaliable.isEmpty()) {
 	            		System.out.println("Não há currículos que se encaixem nos padrões da pesquisa");
 	            		System.out.println("Se você quiser se inscrever para receber vagas futuras, escolha a opção 4");
-	            	}
-	            	else {
-	            		for (int i=0; i<curriculumAvaliable.size(); i++) {
+	            	} else {
+	            		for (int i = 0; i < curriculumAvaliable.size(); i++) {
 	            			System.out.println("Currículo de número " + (i+1));
 	            			System.out.println(" - Nome : " + curriculumAvaliable.get(i).getName());
 	            			System.out.println(" - Contato: " + curriculumAvaliable.get(i).getContact());
@@ -154,23 +136,23 @@ public class CompanyClient {
 	            		
 	            	}
 	            }
-	            else if(option==4) {	//4)Cadastrar para envio de novos currículos
+
+				// 4) Cadastrar para envio de novos currículos
+	            else if(option == 4) {
 	            	System.out.println("Qual a área de interesse que você gostaria de cadastrar?");
-	            	input = keyboard.next();
+	            	input = keyboard.nextLine();
 	            	String jobArea = input;
 	            	try {
 	            		serverInterface.subscribeForJobApplicants(client, jobArea);
-	            	}catch (Exception e) {
+	            	} catch (Exception e) {
 	                    System.out.println("Exception em subscribeForJobApplicants: " + e.getMessage());
 	                }
 	            }
-	            else if(option==5) {
+
+	            else if(option == 5) {
 	            	mayEnd = true;
 	            }
             }
-            
-            
-            
         } catch (RemoteException e) {
             System.out.println("RemoteException ao iniciar o cliente: " + e.getMessage());
         } catch (NotBoundException e) {
