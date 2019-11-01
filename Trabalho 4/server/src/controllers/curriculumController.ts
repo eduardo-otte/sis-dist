2f615afa730651ae97f3ce70673101e116a9f982
Eduardo Otte <eduardo@otte.net.br>
2019-11-01 14:57:11 -0300

    Add collection interfaces and protypes for register functions

--- Trabalho 4/server/src/controllers/curriculumController.ts
+++ Trabalho 4/server/src/controllers/curriculumController.ts
@@ -1,9 +1,18 @@
 import { Request, Response } from "express";
 import { Curriculum } from "../entities/curriculum";
 
+interface CurriculumCollection {
+    [key: string]: any;
+}
+
 class CurriculumController {
+    static curriculums: CurriculumCollection = {};
+
     static find = async (req: Request, res: Response) => {
-        // find a curriculum
+        res.status(200).send(CurriculumController.curriculums);
+    }
+
+    static register = async (req: Request, res: Response) => {
         let curriculum: Curriculum = {
             area: "Test area",
             contact: "Test contact",
@@ -12,7 +21,14 @@ class CurriculumController {
             workload: 40
         };
 
-        res.status(200).send(curriculum);
+        const { name, area } = curriculum;
+
+        if(!CurriculumController.curriculums[area]) {
+            CurriculumController.curriculums[area] = {};
+        }
+
+        CurriculumController.curriculums[area][name] = curriculum;
+        return res.status(200).send();
     }
 }
 
--- Trabalho 4/server/src/controllers/jobOfferController.ts
+++ Trabalho 4/server/src/controllers/jobOfferController.ts
@@ -1,17 +1,34 @@
 import { Request, Response } from "express";
 import { JobOffer } from "../entities/jobOffer";
 
+interface JobOfferCollection {
+    [key: string]: any;
+}
+
 class JobOfferController {
+    static jobOffers: JobOfferCollection = {};
+
     static find = async (req: Request, res: Response) => {
+        return res.status(200).send(JobOfferController.jobOffers);
+    }
+
+    static register = async (req: Request, res: Response) => {
         const jobOffer: JobOffer = {
             area: "Test area",
             companyName: "Test & Testers Inc.",
             contact: "Test contact",
             salary: 100000,
             workload: 10000
+        };
+
+        const { companyName, area } = jobOffer;
+
+        if(!JobOfferController.jobOffers[area]) {
+            JobOfferController.jobOffers[area] = {};
         }
 
-        return res.status(200).send(jobOffer);
+        JobOfferController.jobOffers[area][companyName] = jobOffer;
+        return res.status(200).send();
     }
 }
 
