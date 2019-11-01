ce29071ce5e2827702f32c1446a76b4fcd0c1429
Eduardo Otte <eduardo@otte.net.br>
2019-10-31 23:07:07 -0300

    Create routing structures

--- Trabalho 4/server/src/routes/curriculum.ts
+++ Trabalho 4/server/src/routes/curriculum.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import CurriculumController from "../controllers/curriculumController";
 
 const router = Router();
 
-router.get("/get", CurriculimController.find);
+router.get("/get", CurriculumController.find);
 
 export default router;
--- Trabalho 4/server/src/routes/jobOffer.ts
+++ Trabalho 4/server/src/routes/jobOffer.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import JobOfferController from "../controllers/jobOfferController";
 
 const router = Router();
 
