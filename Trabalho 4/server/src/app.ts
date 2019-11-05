39e35b72c99ef3c81590feb2e840fb1225d6c1cc
Eduardo Otte <eduardo.otte@ebanx.com>
2019-11-05 14:46:21 -0300

    Add body parsers

--- Trabalho 4/server/src/app.ts
+++ Trabalho 4/server/src/app.ts
@@ -1,11 +1,14 @@
 import http from "http";
 import express from "express";
 import routes from "./routes";
+import bodyParser from "body-parser";
 
 const router = express();
 const { PORT = 3000 } = process.env;
 const server = http.createServer(router);
 
+router.use(bodyParser.urlencoded({ extended: false }))
+router.use(bodyParser.json())
 router.use("/", routes);
 
 server.listen(PORT, () =>
