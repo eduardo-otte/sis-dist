eef8ceb8701bc7dcd20363d4e25e0fde34cd5397
Eduardo Otte <eduardo@otte.net.br>
2019-11-12 15:11:04 -0300

    Main test file

--- Trabalho 4/client-applicant/main.py
+++ Trabalho 4/client-applicant/main.py
@@ -3,9 +3,25 @@ import rest_operations as ro
 url = 'http://localhost:3000'
 
 def main():
-    response = ro.get('jobOffer', { })
+    """
+    companyName": "Test McTest III",
+	"area": "Fish hunting",
+	"contact": "Smoke my dudes",
+	"salary": 5000,
+	"workload": 40"""
 
-    print(response)
+    aux = {'companyName': "MyCompany", 'area': "MyArea", 'contact': "MyContact", 'salary': 1000,'workload':30} 
+    aux2 = {'companyName': "MyCompany", 'area': "MyArea", 'contact': "MyContact", 'salary': 10000,'workload':20} 
+
+    response = ro.register('jobOffer', aux)
+    response = ro.register('jobOffer', aux2)
+
+
+    aux1 = {'id': 1, 'salary': 2000,'workload':20} 
+
+    response = ro.update('jobOffer', aux1)
+    
+
 
 if __name__ == "__main__":
     main()
