ab3f0a6ab9f717155069dcd3ac3cad04a257834f
Eduardo Otte <eduardo@otte.net.br>
2019-11-12 15:10:57 -0300

    Fix REST operations for register and update methods

--- Trabalho 4/client-applicant/rest_operations.py
+++ Trabalho 4/client-applicant/rest_operations.py
@@ -35,62 +35,47 @@ def get(entity, filter_parameters):
         print("Request return status: " + response.status_code)
 
 
-def register(entity, filter_parameters):
-    query_string = '?'
-    for key in filter_parameters.keys():
-        query_string += str(key).replace(' ', '%20')
-        query_string += '='
-        query_string += str(filter_parameters[key]).replace(' ', '+')
-        query_string += '&'
-
-    request_url = url + '/' + entity + '/post' + query_string
+def register(entity, data):
+    request_url = url + '/' + entity + '/register'
 
     try:
-        response = r.post(request_url)
+        response = r.post(request_url, json = data)
     except:
         print("Error while performing POST")
 
     if response.status_code == 200:
         print("POST performed successfully")
-        parsed_response = json.loads(response.text)
-        print(parsed_response)
-
-        return parsed_response
+        print(response.text)
+        return response.text
 
     elif response.status_code == 400:
-        print("Bad request")
+        print("Bad request: " + response.text)
 
     else:
         print("Request return status: " + response.status_code)
+        print(response.text)
 
 
-def update(entity, filter_parameters):
-    query_string = '?'
-    for key in filter_parameters.keys():
-        query_string += str(key).replace(' ', '%20')
-        query_string += '='
-        query_string += str(filter_parameters[key]).replace(' ', '+')
-        query_string += '&'
-
-    request_url = url + '/' + entity + '/post' + query_string
+def update(entity, data):
+    request_url = url + '/' + entity + '/update'
 
     try:
-        response = r.post(request_url)
+        response = r.post(request_url, data)
     except:
         print("Error while performing POST")
 
     if response.status_code == 200:
         print("POST performed successfully")
-        parsed_response = json.loads(response.text)
-        print(parsed_response)
+        print(response.text)
 
-        return parsed_response
+        return response.text
 
     elif response.status_code == 400:
-        print("Bad request")
+        print("Bad request: " + response.text)
 
     elif response.status_code == 404:
         print("Not found")
 
     else:
         print("Request return status: " + response.status_code)
+        print(response.text)
