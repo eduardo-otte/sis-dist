6387615fda9f09ab3652c714881cdc8f9c85b234
Eduardo Otte <eduardo@otte.net.br>
2019-11-13 18:54:01 -0300

    Refactor client applicant main

--- Trabalho 4/client-applicant/main.py
+++ Trabalho 4/client-applicant/main.py
@@ -4,11 +4,12 @@ from types import *
 url = 'http://localhost:3000'
 
 def main():
-    opt = 0
-    hasCurriculum = False;
-    curriculum = {'name': "MyName", 'area': "MyArea", 'contact': "MyContact", 'intendedSalary': 1000,'workload':20}
+    selectedOption = 0
+    hasCurriculum = False
+    curriculum = { }
+    curriculumId = None
 
-    while(opt != 4):
+    while(selectedOption != 4):
         print("---------------------------")
         print("Selecione a opção desejada:")
         print("1) Enviar currículo")
@@ -16,64 +17,51 @@ def main():
         print("3) Checar vagas cadastradas")
         print("4) Encerrar")
 
-        opt = int(input())
+        selectedOption = int(input())
 
-        if (opt == 1) and (hasCurriculum) :
+        if (selectedOption == 1) and hasCurriculum:
             print("Você já possui um curriculo cadastrado. Se você quiser atualizar seus dados, selecione a opção 2")
 
-        if (opt == 1) and (hasCurriculum == False) :
+        if (selectedOption == 1) and (not hasCurriculum):
 
-            print("Nome:")
-            name = input();
-            print("Área de Interesse:")
-            area = input();
-            print("Contato:")
-            contact = input();
-            print("Salário pretendido:")
-            intendedSalary = int(input())
-            print("Carga horária pretendida:")
-            workload = int(input())
+            contact = input("Nome: ")
+            area = input("Área de Interesse: ")
+            contact = input("Contato: ")
+            intendedSalary = float(input("Salário pretendido: "))
+            workload = int(input("Carga horária pretendida: "))
 
-            curriculum['name'] = name;
-            curriculum['area'] = area;
-            curriculum['contact'] = contact;
-            curriculum['intendedSalary'] = intendedSalary;
-            curriculum['workload'] = workload;
+            curriculum['name'] = contact
+            curriculum['area'] = area
+            curriculum['contact'] = contact
+            curriculum['intendedSalary'] = intendedSalary
+            curriculum['workload'] = workload
 
-            #print(curriculum)
             hasCurriculum = True
 
-            response = ro.register('curriculum', curriculum)
+            ro.register('curriculum', curriculum)
 
-        elif (opt == 2) and (hasCurriculum == False) :
+        elif (selectedOption == 2) and (not hasCurriculum):
             print("Você não possui um curriculo cadastrado. Para fazer um cadastro, selecione a opção 1")
 
-        elif (opt == 2) and (hasCurriculum == True) :
+        elif (selectedOption == 2) and hasCurriculum:
 
-            print("Você gostaria de atualizar seu contato?S/N")
-            input_ = input()
-            if input_ == 's' or input_ == 'S' :
-                print("Contato:")
-                name = input()
-                curriculum['contact'] = name;
+            curriculum = { }
 
-            print("Você gostaria de atualizar seu salário pretendido?S/N")
-            input_ = input()
-            if input_ == 's' or input_ == 'S' :
-                print("Salário pretendido:")
-                name = int(input())
-                curriculum['intendedSalary'] = name;
+            userInput = input("Você gostaria de atualizar seu contato (S/N)? ")
+            if userInput.lower() == 's':
+                curriculum['contact'] = input("Contato: ")
 
-            print("Você gostaria de atualizar sua carga horária pretendida?S/N")
-            input_ = input()
-            if input_ == 's' or input_ == 'S' :
-                print("Carga horária pretendida:")
-                name = int(input())
-                curriculum['workload'] = name;
+            userInput = input("Você gostaria de atualizar seu salário pretendido (S/N)? ")
+            if userInput.lower() == 's':
+                curriculum['intendedSalary'] = float(input("Salário pretendido: "))
 
-            response = ro.update('jobOffer', curriculum)
-            #print(curriculum)
+            userInput = input("Você gostaria de atualizar sua carga horária pretendida (S/N)? ")
+            if userInput.lower() == 's':
+                curriculum['workload'] = int(input("Carga horária pretendida: "))
 
+            if(curriculum):
+                curriculum['id'] = curriculumId
+                ro.update('jobOffer', curriculum)
 
 if __name__ == "__main__":
     main()
