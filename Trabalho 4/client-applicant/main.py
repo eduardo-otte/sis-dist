3594d0ed56df7bd09dc0f4590a207fbfb2646c33
mpinho <mpinho@alunos.utfpr.edu.br>
2019-11-12 21:25:56 -0300

    Parcial implementatoin of User Interface in client-application

--- Trabalho 4/client-applicant/main.py
+++ Trabalho 4/client-applicant/main.py
@@ -1,26 +1,78 @@
 import rest_operations as ro
+from types import *
 
 url = 'http://localhost:3000'
 
 def main():
-    """
-    companyName": "Test McTest III",
-	"area": "Fish hunting",
-	"contact": "Smoke my dudes",
-	"salary": 5000,
-	"workload": 40"""
+    opt = 0
+    hasCurriculum = False;
+    curriculum = {'name': "MyName", 'area': "MyArea", 'contact': "MyContact", 'intendedSalary': 1000,'workload':20}
 
-    aux = {'companyName': "MyCompany", 'area': "MyArea", 'contact': "MyContact", 'salary': 1000,'workload':30} 
-    aux2 = {'companyName': "MyCompany", 'area': "MyArea", 'contact': "MyContact", 'salary': 10000,'workload':20} 
+    while(opt != 4):
+        print("---------------------------")
+        print("Selecione a opção desejada:")
+        print("1) Enviar currículo")
+        print("2) Alterar dados do currículo")
+        print("3) Checar vagas cadastradas")
+        print("4) Encerrar")
 
-    response = ro.register('jobOffer', aux)
-    response = ro.register('jobOffer', aux2)
+        opt = int(input())
 
+        if (opt == 1) and (hasCurriculum) :
+            print("Você já possui um curriculo cadastrado. Se você quiser atualizar seus dados, selecione a opção 2")
 
-    aux1 = {'id': 1, 'salary': 2000,'workload':20} 
+        if (opt == 1) and (hasCurriculum == False) :
 
-    response = ro.update('jobOffer', aux1)
-    
+            print("Nome:")
+            name = input();
+            print("Área de Interesse:")
+            area = input();
+            print("Contato:")
+            contact = input();
+            print("Salário pretendido:")
+            intendedSalary = int(input())
+            print("Carga horária pretendida:")
+            workload = int(input())
+
+            curriculum['name'] = name;
+            curriculum['area'] = area;
+            curriculum['contact'] = contact;
+            curriculum['intendedSalary'] = intendedSalary;
+            curriculum['workload'] = workload;
+
+            #print(curriculum)
+            hasCurriculum = True
+
+            response = ro.register('curriculum', curriculum)
+
+        elif (opt == 2) and (hasCurriculum == False) :
+            print("Você não possui um curriculo cadastrado. Para fazer um cadastro, selecione a opção 1")
+
+        elif (opt == 2) and (hasCurriculum == True) :
+
+            print("Você gostaria de atualizar seu contato?S/N")
+            input_ = input()
+            if input_ == 's' or input_ == 'S' :
+                print("Contato:")
+                name = input()
+                curriculum['contact'] = name;
+
+            print("Você gostaria de atualizar seu salário pretendido?S/N")
+            input_ = input()
+            if input_ == 's' or input_ == 'S' :
+                print("Salário pretendido:")
+                name = int(input())
+                curriculum['intendedSalary'] = name;
+
+            print("Você gostaria de atualizar sua carga horária pretendida?S/N")
+            input_ = input()
+            if input_ == 's' or input_ == 'S' :
+                print("Carga horária pretendida:")
+                name = int(input())
+                curriculum['workload'] = name;
+
+            response = ro.update('jobOffer', curriculum)
+            #print(curriculum)
 
 
 if __name__ == "__main__":
